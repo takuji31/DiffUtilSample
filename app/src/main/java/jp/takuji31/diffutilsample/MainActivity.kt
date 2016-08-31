c356c602e69ac9c1bcee58f15b57bb2416fa400b
Takuji Nishibayashi <takuji31@gmail.com>
2016-08-31 09:27:41 +0900

    wip

--- app/src/main/java/jp/takuji31/diffutilsample/MainActivity.kt
+++ app/src/main/java/jp/takuji31/diffutilsample/MainActivity.kt
@@ -1,8 +1,15 @@
 package jp.takuji31.diffutilsample
 
+import android.content.Context
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.util.DiffUtil
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
+import android.support.v7.widget.RecyclerView.ViewHolder
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
 import jp.takuji31.diffutilsample.Status.*
 
 enum class Status {
@@ -47,10 +54,57 @@ class DiffCallback(val oldList: List<Artist>, val newList: List<Artist>) : DiffU
     }
 }
 
+class Adapter() : RecyclerView.Adapter<Adapter.ViewHolder>() {
+
+    var items = listOf<Artist>()
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val item = items[position]
+
+        holder.nameTextView.text = item.name
+        holder.statusTextView.text = when (item.status) {
+            Status.INTERESTED -> ""
+            Status.LIKE -> "Like"
+            Status.LOVE -> "Love"
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder(View.inflate(parent.context, R.layout.recycler_view_row, parent))
+    }
+
+    override fun getItemCount(): Int {
+        return items.count()
+    }
+
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val nameTextView by lazy {
+            itemView.findViewById(R.id.nameTextView) as TextView
+        }
+        val statusTextView by lazy {
+            itemView.findViewById(R.id.statusTextView) as TextView
+        }
+    }
+}
+
 class MainActivity : AppCompatActivity() {
+
+    val recylerView by lazy {
+        findViewById(R.id.recyclerView) as RecyclerView
+    }
+
+    val adapter by lazy {
+        Adapter()
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        recylerView.layoutManager = LinearLayoutManager(this)
+        recylerView.adapter = adapter
+
+        adapter.items = Artist.list
+        adapter.notifyDataSetChanged()
     }
 }
